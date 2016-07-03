c16259fadc6800870ace11e829b0ccb3bf1aa6e4
skulikowski <szymkul@gmail.com>
2016-07-03 14:28:27 +0200

    first commit

---
+++ output/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var months = Object.freeze(['jan', 'feb']);
+months.push('element1');
+
+console.log(months);
---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const months = Object.freeze(['jan', 'feb']);
+months.push('element1');
+
+console.log(months);
